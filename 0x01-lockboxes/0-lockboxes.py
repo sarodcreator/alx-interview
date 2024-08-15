69567472ba0b11a684f03a8fe66cc3d0559214cc
Baridoo Saro <sarobaridoo@gmail.com>
2024-08-15 15:07:49 +0000

    fixed

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,4 +1,9 @@
+#!/usr/bin/python3
+"""Module to determine if a list of boxes contain keys to other boxes"""
+
+
 def canUnlockAll(boxes):
+    """Function to determine keys from list of boxes"""
     n = len(boxes)
     confirm = True
 

4d6f080da62d172edd45f66d512d3aca47cf1181
Baridoo Saro <sarobaridoo@gmail.com>
2024-08-15 15:16:56 +0000

    fixed

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,15 +1,12 @@
 #!/usr/bin/python3
 """Module to determine if a list of boxes contain keys to other boxes"""
 
-
 def canUnlockAll(boxes):
     """Function to determine keys from list of boxes"""
     n = len(boxes)
     confirm = True
-
     for key in range(1, n - 1):
         visited = False
-        
         for box in range(n):
             visited = key in boxes[box] and key != box
             if visited == confirm:

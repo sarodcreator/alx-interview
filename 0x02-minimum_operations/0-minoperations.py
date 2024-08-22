f58e4bbe7a74010ae11bbc0192aea722d6ada27f
Baridoo Saro <sarobaridoo@gmail.com>
2024-08-22 23:26:34 +0000

    fixed

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -3,28 +3,28 @@
 """
 
 
-def min_operations(n):
-    """Calculate the fewest number of operations needed to result in
-    exactly n H characters in the file.
+def minOperations(n):
+     """Calculates the fewest number of operations needed to result in
+     exactly n H characters in the file.
 
-    Args:
-        n (int): The number of desired H characters.
+     Args:
+         n (int): The number of desired H characters.
 
-    Returns:
-        int: The minimum number of operations needed to get n H characters,
-             or 0 if it is impossible to achieve n.
-    """
-    if not isinstance(n, int) or n <= 1:
-        return 0
+     Returns:
+         int: The number of minimal operations needed to get n H characters
+               or 0 if it is impossible to achieve n.
+     """
+     if not isinstance(n, int) or n <= 1:
+         return 0
 
-    ops_count = 0
-    divisor = 2
+     ops_count = 0
+     divisor = 2
 
-    while n > 1:
-        while n % divisor == 0:
-            ops_count += divisor
-            n //= divisor
-        divisor += 1
+     while n > 1:
+         while n % divisor == 0:
+             ops_count += divisor
+             n //= divisor
+         divisor += 1
 
-    return ops_count
+     return ops_count
 

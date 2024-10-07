550bd649d0ee75d3ea802b8c982105866dd12117
Baridoo Saro <sarobaridoo@gmail.com>
2024-10-07 16:35:47 +0000

    fixed

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,26 @@
+#!usr/bin/python3
+"""A function that returns the perimeter
+of the island described in grid"""
+
+
+def island_perimeter(grid):
+    """
+    :type grid: List[List[int]]
+    return int
+    """
+    perimeter = 0
+    connection = 0
+    length_row = len(grid)
+    length_col = len(grid[0])
+
+    for i in range(length_row):
+        for j in range(length_col):
+            if grid[i][j] == 1:
+                perimeter += 4
+
+                """check for top"""
+                if i != 0 and grid[i - 1][j] == 1:
+                    connection += 4 - 2
+                if j != 0 and grid[i][j - 1] == 1:
+                    connection += 4 - 2
+    return perimeter - (connection)
